4ddcc19fdcd596aa233aa8818552a34bca339fad
Kirami78 <whxxtzl69@gmail.com>
2024-11-01 20:54:59 +0700

    Create Koleksi_Array.js

---
+++ Koleksi_Array.js
@@ -0,0 +1,38 @@
+// Create 
+function addToList(list, element) {
+    list.push(element);
+}
+// Read 
+function readFromList(list, index) {
+    if (index >= 0 && index < list.length) {
+        return list[index];
+    }
+    throw new Error("Indeks di luar jangkauan");
+}
+// Update
+function updateList(list, index, newValue) {
+    if (index >= 0 && index < list.length) {
+        list[index] = newValue;
+    } else {
+        throw new Error("Indeks di luar jangkauan");
+    }
+}
+// Delete
+function deleteFromList(list, index) {
+    if (index >= 0 && index < list.length) {
+        list.splice(index, 1);
+    } else {
+        throw new Error("Indeks di luar jangkauan");
+    }
+}
+// Penggunaan
+let listMahasiswa = ["Budi", "Siti", "Agus"];
+console.log("List mahasiswa :");
+console.log(listMahasiswa);
+addToList(listMahasiswa, "Ani");
+console.log("C : ",listMahasiswa);
+console.log("R : ",readFromList(listMahasiswa, 1));
+updateList(listMahasiswa, 2, "Agung");
+console.log("U : ",listMahasiswa);
+deleteFromList(listMahasiswa, 0);
+console.log("D : ",listMahasiswa); 
